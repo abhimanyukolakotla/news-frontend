a42fb85b0df331cd0f19675aaedd501cb825b0b4
Abhimanyu Kolakotla19 <abhimanyu.kolakotla@in.ibm.com>
2022-02-21 17:53:55 +0530

    Extracted base url logic

--- src/services/NewsApiService.js
+++ src/services/NewsApiService.js
@@ -1,17 +1,14 @@
 class NewsApiService {
     baseUrl = process.env.REACT_APP_BACKEND_SERVICE_URL + ":" + process.env.REACT_APP_BACKEND_SERVICE_PORT + "/api";
+    NewsApiService() {
+        this.baseUrl = process.env.REACT_APP_BACKEND_SERVICE_URL === undefined? "api": this.baseUrl;
+    }
     async fetchTopNews() {
-        if(process.env.REACT_APP_BACKEND_SERVICE_URL === undefined) {
-            this.baseUrl = "api";
-        }
         var response =  await fetch(`${this.baseUrl}/topstories.json`);
         return response.json();
     }
 
     async fetchNewsItem(id) {
-        if(process.env.REACT_APP_BACKEND_SERVICE_URL === undefined) {
-            this.baseUrl = "";
-        }
         var response =  await fetch(`${this.baseUrl}/item/${id}.json`);
         return response.json();
     }
