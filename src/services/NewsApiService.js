f88abbe4a03847f273bba7c7a1556c7fcb3240da
Abhimanyu Kolakotla19 <abhimanyu.kolakotla@in.ibm.com>
2022-02-21 16:54:33 +0530

    Modified code to fix baseurl

--- src/services/NewsApiService.js
+++ src/services/NewsApiService.js
@@ -1,6 +1,9 @@
 class NewsApiService {
     baseUrl = process.env.REACT_APP_BACKEND_SERVICE_URL + ":" + process.env.REACT_APP_BACKEND_SERVICE_PORT + "/api";
     async fetchTopNews() {
+        if(process.env.REACT_APP_BACKEND_SERVICE_URL == undefined) {
+            this.baseUrl = "";
+        }
         var response =  await fetch(`${this.baseUrl}/topstories.json`);
         return response.json();
     }
